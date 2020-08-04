54c66d700b21b3b9b568973d99c32f7fd7829606
Hardik Dholakiya <dholakiyahardik10@gmail.com>
2020-08-04 18:12:41 +0530

    Fixed import statement issue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import Textbox from './components/Textbox.vue'
 import Selectbox from './components/Selectbox.vue'
-const components = {
-    Textbox, Selectbox
-}
-export default components
+
+export { Textbox, Selectbox }
